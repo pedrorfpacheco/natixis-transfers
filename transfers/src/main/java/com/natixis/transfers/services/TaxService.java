e8137db9e46d65362ff958362b92e6595d524fb5
Pedro Pacheco <148588945+pedrorfpacheco@users.noreply.github.com>
2024-02-29 15:53:41 +0000

    Use constants to be more easy to change in the future the values of tax percentages

--- transfers/src/main/java/com/natixis/transfers/services/TaxService.java
+++ transfers/src/main/java/com/natixis/transfers/services/TaxService.java
@@ -13,6 +13,17 @@ public class TaxService {
     private static final double LIMIT_TAX_A = 1000.0;
     private static final double LIMIT_TAX_B = 2000.0;
 
+    private static final double PERCENTAGE_TAX_A = 0.03;
+    private static final double VALUE_TAX_A = 3;
+
+    private static final double PERCENTAGE_TAX_B = 0.09;
+
+    private static final double PERCENTAGE_TAX_C1 = 0.082;
+    private static final double PERCENTAGE_TAX_C2 = 0.069;
+    private static final double PERCENTAGE_TAX_C3 = 0.047;
+    private static final double PERCENTAGE_TAX_C4 = 0.017;
+
+
     protected Tax calculateTax(final long value, final LocalDate date) {
         if (value <= LIMIT_TAX_A) {
             return calculateValueTax(TypeTax.A, value, date);
@@ -34,27 +45,27 @@ public class TaxService {
         switch (type) {
             case A:
                 if (diffDays == 0) {
-                    return new Tax(type, 0.03 * value + 3);
+                    return new Tax(type, PERCENTAGE_TAX_A * value + VALUE_TAX_A);
                 } else {
-                    throw new IllegalArgumentException("Invalid schedule date for Type Tax A:" + date + " it has to be the current date.");
+                    throw new IllegalArgumentException("Invalid schedule date for Type Tax A: " + date + " it has to be the current date.");
                 }
             case B:
                 if (diffDays >= 1 && diffDays <= 10) {
-                    return new Tax(type, 0.09 * value);
+                    return new Tax(type, PERCENTAGE_TAX_B * value);
                 } else {
-                    throw new IllegalArgumentException("Invalid schedule date for Type Tax B:" + date + " it has to be between 1 and 10 days after the current date.");
+                    throw new IllegalArgumentException("Invalid schedule date for Type Tax B: " + date + " it has to be between 1 and 10 days after the current date.");
                 }
             case C:
                 if (diffDays >= 11 && diffDays <= 20) {
-                    return new Tax(type, 0.082 * value);
+                    return new Tax(type, PERCENTAGE_TAX_C1 * value);
                 } else if (diffDays >= 21 && diffDays <= 30) {
-                    return new Tax(type, 0.069 * value);
+                    return new Tax(type, PERCENTAGE_TAX_C2 * value);
                 } else if (diffDays >= 31 && diffDays <= 40) {
-                    return new Tax(type, 0.047 * value);
+                    return new Tax(type, PERCENTAGE_TAX_C3 * value);
                 } else if (diffDays > 40) {
-                    return new Tax(type, 0.017 * value);
+                    return new Tax(type, PERCENTAGE_TAX_C4 * value);
                 } else {
-                    throw new IllegalArgumentException("Invalid schedule date for Type Tax C:" + date + " it has to be at least 11 days from today");
+                    throw new IllegalArgumentException("Invalid schedule date for Type Tax C: " + date + " it has to be at least 11 days from today");
                 }
             default:
                 throw new IllegalArgumentException("Type of Tax invalid: " + type);
