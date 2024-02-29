ea094aac5486ddfb036e28878c1533ca26ed3f42
Pedro Pacheco <148588945+pedrorfpacheco@users.noreply.github.com>
2024-02-29 13:05:32 +0000

    create Tax domain class

---
+++ transfers/src/main/java/com/natixis/transfers/domain/Tax.java
@@ -0,0 +1,16 @@
+package com.natixis.transfers.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tax {
+
+    private TypeTax typeTax;
+
+
+}
