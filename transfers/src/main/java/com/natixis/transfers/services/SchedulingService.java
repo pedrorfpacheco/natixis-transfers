42c4e30e9300f67c718af8fb9ea84897487676f4
Pedro Pacheco <148588945+pedrorfpacheco@users.noreply.github.com>
2024-02-29 15:14:11 +0000

    Finalize method of creation/getting on service of Transfers

--- transfers/src/main/java/com/natixis/transfers/services/SchedulingService.java
+++ transfers/src/main/java/com/natixis/transfers/services/SchedulingService.java
@@ -1,13 +1,40 @@
 package com.natixis.transfers.services;
 
+import com.natixis.transfers.domain.Tax;
+import com.natixis.transfers.domain.Transfer;
+import com.natixis.transfers.repos.TransferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class SchedulingService {
 
     @Autowired
-    private SchedulingService schedulingService;
+    private TransferRepository transferRepository;
+
+    @Autowired
+    private TaxService taxService;
+
+    public Transfer scheduleTransfer(final long value, final LocalDate date) {
+        final Tax tax = taxService.calculateTax(value, date);
+
+        final Transfer transfer = new Transfer(null, value, LocalDate.now(), date, tax);
+
+        try {
+            transferRepository.save(transfer);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error saving transfer: " + e.getMessage());
+        }
+
+        return transfer;
+    }
+
+    public List<Transfer> getTransfers() {
+        return transferRepository.findAll();
+    }
 
 
 }
