4d6b2a40f3aa626c5a66821afe0a1271159aa497
Pedro Pacheco <148588945+pedrorfpacheco@users.noreply.github.com>
2024-02-29 15:16:33 +0000

    Create a method on repo to get by date a transfer

--- transfers/src/main/java/com/natixis/transfers/repos/TransferRepository.java
+++ transfers/src/main/java/com/natixis/transfers/repos/TransferRepository.java
@@ -4,6 +4,10 @@ import com.natixis.transfers.domain.Transfer;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface TransferRepository extends MongoRepository<Transfer, String> {
+    List<Transfer> findByDate(LocalDate date);
 }
