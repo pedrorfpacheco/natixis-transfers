053b37bf82b70463d99cd4a7ce0bd65ef5642401
Pedro Pacheco <148588945+pedrorfpacheco@users.noreply.github.com>
2024-02-29 15:15:10 +0000

    Create endpoint to create a transfer and get all transfers.

--- transfers/src/main/java/com/natixis/transfers/controllers/SchedulingController.java
+++ transfers/src/main/java/com/natixis/transfers/controllers/SchedulingController.java
@@ -1,9 +1,14 @@
 package com.natixis.transfers.controllers;
 
+import com.natixis.transfers.domain.Transfer;
 import com.natixis.transfers.services.SchedulingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/schedule")
@@ -12,5 +17,25 @@ public class SchedulingController {
     @Autowired
     private SchedulingService schedulingService;
 
+    @PostMapping("/{value}/{dateString}")
+    public ResponseEntity<?> createTransfer(@PathVariable final long value, @PathVariable final String dateString) {
+        LocalDate date = LocalDate.parse(dateString);
 
+        try {
+            Transfer transfer = schedulingService.scheduleTransfer(value, date);
+
+            return new ResponseEntity<>(transfer, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+
+    @GetMapping
+    public ResponseEntity<List<Transfer>> getAllTransfers() {
+        List<Transfer> transfers = schedulingService.getTransfers();
+
+        return new ResponseEntity<>(transfers, HttpStatus.OK);
+    }
 }
